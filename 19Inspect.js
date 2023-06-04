c8a432d50ff56eb1e34fcfa34f54fa6a6bf77527
CPuPush <fpakpahan44@gmail.com>
2023-06-04 22:46:50 +0700

    docker bassic

--- 19Inspect.js
+++ 19Inspect.js
@@ -1,3 +1,20 @@
 /*
+! Inspect
+* Setelah kita men-download image, atau membuat network, volume dan container. Kadang kita ingin melihat detail dari tiap hal tersebut
+* Misal kita ingin melihat detail dari image, perintah apa yang digunakan oleh image tersebut? Environment variable apa yang digunakan? Atau port apa yang digunakan?
+* Misal kita juga ingin melihat detail dari container, Volume apa yang digunakan? Environment variable apa yang digunakan? Port forwarding apa yang digunakan? dan lain-lain
+* Docker memiliki fitur bernama inspect, yang bisa digunakan di image, container, volume dan network
+* Dengan fitur ini, kita bisa melihat detail dari tiap hal yang ada di Docker
+
+? Menggunakan Inspect
+* untuk melihat detail dari image, gunakan:
+todo     docker image inspect namaImage
+* detail dari container
+todo     docker container inpect namaContainer
+* detail dari volume
+todo     docker volume inspect namaVolume
+* detail dari network
+todo     docker network inspect namaNetwork
+
 
 */
